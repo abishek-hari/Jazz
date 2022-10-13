a41dbd5903cc62f59a99f96bcda29ff7465dd748
Abishek <abishekhari09@gmail.com>
2022-10-13 20:00:44 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+  {
+    name: "saami saami",
+    path: "assets/musics/saami saami (Tamil).mp3",
+    artist: "mounika yadav",
+    cover: "assets/images/puspa.jpg",
+  },
+  {
+    name: "arabic kuthu",
+    path: "assets/musics/Arabic Kuthu.mp3",
+    artist: "aniruth ravichandren",
+    cover: "assets/images/beast.jpg",
+  },
+  {
+    name: "megham karukatha",
+    path: "assets/musics/Megham Karukatha.mp3",
+    artist: "dhanush",
+    cover: "assets/images/cover 14.jpg",
+  },
+  {
+    name: "kanja poovu kannala",
+    path: "assets/musics/kanja poovu kannala.mp3",
+    artist: "sid sriram",
+    cover: "assets/images/viruman.jpg",
+  },
+  {
+    name: "pathala pathala",
+    path: "assets/musics/pathala pathala.mp3",
+    artist: "kamal hassan",
+    cover: "assets/images/Vikram.jpg",
+  },
+  {
+    name: "srivalli",
+    path: "assets/musics/srivalli - Tamil.mp3",
+    artist: "sid sriram",
+    cover: "assets/images/puspa.jpg",
+  },
+  {
+    name: "Thaai Kelavi",
+    path: "assets/musics/Thaai Kelavi.mp3",
+    artist: "dhanush",
+    cover: "assets/images/cover 14.jpg",
+  },
+  {
+    name: "Thenmozhi",
+    path: "assets/musics/Thenmozhi.mp3",
+    artist: "dhanush",
+    cover: "assets/images/cover 14.jpg",
+  },
+];
